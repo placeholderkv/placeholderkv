28e7091bcb467485177b37d9e0229c61bc80e7cb
Binbin <binloveplay1314@qq.com>
2025-02-11 17:15:36 +0800

    Fix new benchmark test under the TLS
    We need to call cliSecureConnection if we are runing under
    the TLS, otherwise the connection can not be use.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- tests/integration/valkey-benchmark.tcl
+++ tests/integration/valkey-benchmark.tcl
@@ -12,6 +12,9 @@ proc common_bench_setup {cmd} {
     if {[catch { exec {*}$cmd } error]} {
         set first_line [lindex [split $error "\n"] 0]
         puts [colorstr red "valkey-benchmark non zero code. first line: $first_line"]
+        if {$::verbose} {
+            puts [colorstr red "valkey-benchmark non zero code, the output is: $error"]
+        }
         fail "valkey-benchmark non zero code. first line: $first_line"
     }
 }
